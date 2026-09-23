c3c46cce67c3410ae69e1d9983013566e9a7b99a
minesettimi <minesettimi@gmail.com>
2026-09-23 01:04:09 -0400

    implemented loot spawns

--- Server/CuriosMod.cs
+++ Server/CuriosMod.cs
@@ -19,6 +19,7 @@ public class CuriosMod(JsonUtil jsonUtil,
     WTTCustomItemParentService itemParentService,
     WTTCustomItemServiceExtended itemServiceExtended,
     WTTCustomLocaleService localeService,
+    WTTCustomLootspawnService lootspawnService,
     IEnumerable<IRuntimePatch> patches,
     RagfairConfig ragfairConfig,
     ISptLogger<CuriosMod> logger) : IOnLoad
@@ -48,5 +49,6 @@ public class CuriosMod(JsonUtil jsonUtil,
         await itemParentService.CreateCustomParents(Assembly, "db/Parents");
         await itemServiceExtended.CreateCustomItems(Assembly, "db/Items");
         await localeService.CreateCustomLocales(Assembly, "db/Locales");
+        await lootspawnService.CreateCustomLootSpawns(Assembly, "db/LooseLoot");
     }
 }
